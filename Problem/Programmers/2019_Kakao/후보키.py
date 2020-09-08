6523b640673749698b7695b989b6a3130d471174
SeyoungJeon <seyoung0254@hanmail.net>
2020-09-08 12:42:26 +0900

    [Modify] '후보키' 해설 조금 참고해서 다시 품 부분 집합 확인 (s1 == s1.intersection(s2)) 조합 경우의 수 중에서 최대 길이까지 포함시키기

--- Problem/Programmers/2019_Kakao/후보키.py
+++ Problem/Programmers/2019_Kakao/후보키.py
@@ -20,42 +20,40 @@ from itertools import combinations
 
 
 def solution(relation):
-    answer = 0
-
     elements_list = []
     idx_list = [i for i in range(len(relation[0]))]
-
-    for i in range(1, len(relation[0])):
+    
+    for i in range(1, len(relation[0]) + 1):
         combinations_list = list(combinations(idx_list, i))
-
         for elements in combinations_list:
             data_set = set()
             for value in relation:
                 data_set.add(tuple([value[element] for element in elements]))
-
             if len(data_set) == len(relation):
-                elements_list.append([elements, False])
+                for idx1 in range(0, len(elements_list)):
+                    s1 = set(elements_list[idx1])
+                    if s1 == s1.intersection(set(elements)):
+                        break
+                else:
+                    elements_list.append(elements)
 
-    for idx in range(0, len(elements_list)):
-        if elements_list[idx][1]:
-            continue
-
-        for idx2 in range(idx+1, len(elements_list)):
-            for element in elements_list[idx][0]:
-                if element in elements_list[idx2][0]:
-                    elements_list[idx2][1] = True
-                    break
-    for value in elements_list:
-        if not value[1]:
-            answer += 1
-
-    return answer
+    return len(elements_list)
 
 """
 @ 세뚱이 풀이
 1. combinations을 활용해 조합 경우의 수를 모두 구한다.
 2. len()과 set()을 이용해 중복이 되었는지 안되었는지 확인한다.
 3. 중복이 안되는 조합만 리스트로 관리하여, 최소성을 만족하도록 수정했다.
+
+@ 수정
+1. 조합 경우의 수 중에서 최대 길이인것도 포함하게 함
+
+@ 해설 풀이 반영
+1. 부분 집합인지 확인하기 (if s1 == s1.intersection(set(elements)):)
 """
 
+"""
+Test Case
+
 print(solution([["100", "ryan", "music", "2"], ["200", "apeach", "math", "2"], ["300", "tube", "computer", "3"], ["400", "con", "computer", "4"], ["500", "muzi", "music", "3"], ["600", "apeach", "music", "2"]]))
+"""
