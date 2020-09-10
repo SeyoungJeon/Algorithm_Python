c9915cde171c8607ea2e8d78ebb279ebab6d40f5
SeyoungJeon <seyoung0254@hanmail.net>
2020-09-10 12:51:19 +0900

    [Modify] '문자열 압축' 다른 사람 풀이 반영하여 수정 길이에 따라 문자열 자르고, 해당 문자열 리스트와 1개씩 앞당긴 문자열 리스트를 비교하며 압축된 부분 세기

--- Problem/Programmers/2020_Kakao/문자열_압축.py
+++ Problem/Programmers/2020_Kakao/문자열_압축.py
@@ -17,19 +17,19 @@ def solution(s):
     max_len = len(s) // 2 if len(s) % 2 == 0 else len(s) // 2 + 1
     for i in range(1, max_len+1):
         string_list = [s[j:j+i] for j in range(0, len(s), i)]
-        compression_string = ''
-        value, count = string_list[0], 1
-        for idx in range(1, len(string_list)):
-            if value == string_list[idx]:
+        compression_string, count = '', 1
+        cur_char = string_list[0]
+        for value1, value2 in zip(string_list, string_list[1:] + ['']):
+            if value1 == value2:
                 count += 1
             else:
-                compression_string += str(count) + value if count > 1 else value
+                compression_string += str(count) + cur_char if count > 1 else cur_char
                 count = 1
-                value = string_list[idx]
-        compression_string += str(count) + value if count > 1 else value
+                cur_char = value2
         answer = min(answer, len(compression_string))
 
     return answer
+
 
 """
 @ 세뚱이 풀이
@@ -37,9 +37,18 @@ def solution(s):
 2. 1 부터 자를 수 있는 최대 길이까지 반복하면서 문자열 잘라서 저장
 3. 자른 문자열을 기준으로 원래 문자열과 비교하며 압축 문자의 개수 세기
 4. 압축 개수 세어서 압축 문자열 만든 후, 최솟값 저장
+
+@ 다른 사람 풀이
+1. 길이에 따라 자른 문자열 리스트와 해당 문자열 리스트를 하나씩 당긴 리스트랑 비교
+2. 압축된 부분 개수 세기
 """
+
+"""
+Test Case
+
 print(solution("aabbaccc"))
 print(solution("ababcdcdababcdcd"))
 print(solution("abcabcdede"))
 print(solution("abcabcabcabcdededededede"))
 print(solution("xababcdcdababcdcd"))
+"""
