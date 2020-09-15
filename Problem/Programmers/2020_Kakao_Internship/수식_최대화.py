174bebad816f13ae11a1c4ae5f596e218e7e2d22
SeyoungJeon <seyoung0254@hanmail.net>
2020-09-15 17:31:18 +0900

    [Add] '수식 최대화' 다른 사람 풀이 중 반영할 점 list 함수 중 index 활용 수식 계산 중 2가지 방법 있음 (eval 함수, dictionary로 관리)

--- Problem/Programmers/2020_Kakao_Internship/수식_최대화.py
+++ Problem/Programmers/2020_Kakao_Internship/수식_최대화.py
@@ -2,12 +2,8 @@ from itertools import permutations
 
 
 def calculate(num1, num2, operation):
-    if operation == '-':
-        return num1 - num2
-    elif operation == '+':
-        return num1 + num2
-    else:
-        return num1 * num2
+    operation_dict = {'-': num1-num2, '+': num1+num2, '*': num1*num2}
+    return operation_dict[operation]
 
 
 def solution(expression):
@@ -30,15 +26,13 @@ def solution(expression):
     for element in permutations(operation_set):
         temp_expression_list = [value for value in expression_list]
         for operation in element:
-            idx = 1
             while operation in temp_expression_list:
+                idx = temp_expression_list.index(operation)
                 if temp_expression_list[idx] == operation:
                     value = calculate(temp_expression_list[idx-1], temp_expression_list[idx+1], operation)
                     for i in range(3):
                         temp_expression_list.pop(idx-1)
                     temp_expression_list.insert(idx-1, value)
-                    idx -= 1
-                idx += 1
 
         if answer < abs(temp_expression_list[0]):
             answer = abs(temp_expression_list[0])
@@ -50,6 +44,12 @@ def solution(expression):
 1. expression을 연산자 기준으로 분리된 리스트 관리
 2. expression에 포함된 연산자를 permutation 구하기
 3. 우선순위 순열에 따라 계산하며 최종값 중 절대값이 제일 큰 수 저장
+
+@ 다른 사람 풀이 중 반영할 점
+1. list 함수 중 index 활용하기
+2. 문자열 계산 시 2가지 방법
+    - eval 함수 사용
+    - dictionary로 계산 결과 값 관리 
 """
 
 print(solution("0-0*0-0+09"))
